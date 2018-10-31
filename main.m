b4d9d22ddc1c422d8ab43f50b070f3daf4b21d0e
PrajwalShingala <shingalaprajwal@gmail.com>
2018-11-01 03:24:13 +0530

    spliting image done

--- main.m
+++ main.m
@@ -8,12 +8,11 @@ yiq_image  = rgb2ntsc(rgb_img);
 % Retaining Q chromaticity
 q_com = yiq_image(:,:,3);
 % imtool(q_com);
-
+sz = size(q_com);
 %% Otsu's thresholding
 level = graythresh(q_com);
 BW = imbinarize(q_com,level);
 % imtool(BW);
-sz = size(BW);
 num = floor(sz(2)/10);
 std = zeros(1,sz(2));
 mn = mean(BW);
@@ -30,6 +29,30 @@ Ry = cp(1,num);
 [max_num_r,Rx] = max(q_com(:,Ry));
 
 %% Dividing image into uppper and lower part
-
-
+x1 = Ly;
+x2 = Ry;
+y1 = Lx;
+y2 = Rx;
+coefficients = polyfit([x1, x2], [y1, y2], 1);
+a = coefficients (1);
+b = coefficients (2);
+Q_upper = zeros(sz);
+Q_lower = zeros(sz);
+for x = 1:sz(2)
+    for y = 1:sz(1)
+        if (a*x + b - y) > 0
+            Q_upper(y,x) = q_com(y,x);
+        else
+            Q_lower(y,x) = q_com(y,x);
+        end
+    end
+end
 %% Thresholding
+
+%% Contour
+SE_closing = strel('disk',10);
+SE_opening = strel('disk',5);
+img = imclose(BW,SE_closing);
+img = imopen(img,SE_opening);
+% imshow(img);
+% imcontour(img);
